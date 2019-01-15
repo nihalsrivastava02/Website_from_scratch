5ed61d5f0721c56efae142dd74175ffb75117791
nihalsrivastava02 <43031625+nihalsrivastava02@users.noreply.github.com>
2019-01-15 11:54:24 +0530

    Add files via upload

---
+++ indexjs.js
@@ -0,0 +1,23 @@
+ <script>
+                // Get the modal
+                var modal = document.getElementById('id01');
+
+                // When the user clicks anywhere outside of the modal, close it
+                window.onclick = function(event) {
+                    if (event.target == modal) {
+                        modal.style.display = "none";
+                    }
+                }
+            </script>
+/*this script is for login*/
+ <script>
+                    // Get the modal
+                    var modal = document.getElementById('id02');
+
+                    // When the user clicks anywhere outside of the modal, close it
+                    window.onclick = function(event) {
+                    if (event.target == modal) {
+                    modal.style.display = "none";
+                        }
+                    }
+                </script>  
